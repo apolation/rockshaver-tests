39b1ef4941f847706f295f76c09788dae22ce893
Matheus Bertassi <matheusbs4321@gmail.com>
2025-02-27 00:22:12 -0300

    fix/agendamento

--- web/cypress/e2e/agendamento.cy.js
+++ web/cypress/e2e/agendamento.cy.js
@@ -35,23 +35,7 @@ describe('Agendamentos', () => {
 
         cy.dropCollection('agendamentos', { failSilently: 'true' })
 
-        cy.request({
-            method: 'POST',
-            url: 'http://localhost:3333/api/agendamentos',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer 3a8a9b8fae87baf503e7c5fe5b97fd72',
-            },
-            body: {
-                nomeCliente: agendamento.usuario.nome,
-                emailCliente: agendamento.usuario.email,
-                data: agendamento.data,
-                matricula: agendamento.profissional.matricula,
-                codigoServico: agendamento.servico.codigo
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(201)
-        })
+        cy.agendamentoApi(agendamento)
 
         cy.intercept('GET', 'http://localhost:3333/api/calendario', {
             statusCode: 200,
@@ -67,7 +51,7 @@ describe('Agendamentos', () => {
         cy.escolherDiaAgendamento(agendamento.dia);
         cy.escolherDiaAgendamento(agendamento.dia);
 
-        cy.get('[slot="19:00 - ocupado"]')
+        cy.get(`[slot="${agendamento.hora} - ocupado"]`)
             .should('be.visible')
             .find('svg')
             .should('be.visible')
